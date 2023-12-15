42bd8b6885b273d86892d2b4e24c91b61637c353
Krueger <noah.krueger@gentex.com>
2023-12-15 17:19:43 -0500

    Initial commit of the project.

---
+++ Program.cs
@@ -0,0 +1,150 @@
+﻿using System.Text.Json;
+
+namespace BunkBedBomRouting
+{
+    class BomItem
+    {
+        public string description { get; set; } = "";
+        public int quantity { get; set; }
+        public int step { get; set; } = -1;
+        public string source { get; set; } = "";
+        public BomItem[] bom { get; set; } = Array.Empty<BomItem>();
+    }
+
+    class RoutingItem
+    {
+        public int step { get; set; }
+        public string description { get; set; } = "";
+        public int taktTime { get; set; }
+    }
+
+    class Program
+    {
+        /// <summary>
+        /// Gets all components and subcomponents of the given BOM.
+        /// </summary>
+        /// <param name="bom">BOM object type</param>
+        /// <returns>Array of all components and subcomponents.</returns>
+        static BomItem[] GetAllComponents(BomItem bom)
+        {
+            List<BomItem> list = new List<BomItem>();
+            foreach (BomItem item in bom.bom)
+            {
+                list.Add(item);
+                list.AddRange(GetAllComponents(item));
+            }
+            return list.ToArray();
+        }
+
+        /// <summary>
+        /// Gets all components that contain unprovided subcomponents in its BOM.
+        /// </summary>
+        /// <param name="bom">BOM object type</param>
+        /// <returns>Array of components with unprovided materials.</returns>
+        static BomItem[] GetStepsWithUnprovidedComponents(BomItem bom)
+        {
+            List<BomItem> list = new List<BomItem>();
+            foreach (BomItem item in bom.bom)
+            {
+                // if the component has a BOM and at least one of its subcomponents is not provided
+                if (item.bom.Length > 0 && item.bom.Where(x => !x.source.ToLower().Equals("provided")).Any())
+                {
+                    list.Add(item);
+                    list.AddRange(GetStepsWithUnprovidedComponents(item));
+                }
+            }
+            return list.ToArray();
+        }
+
+        /// <summary>
+        /// Sums the quantities of all components in the given BOM and writes the results to a file.
+        /// </summary>
+        /// <param name="bom">BOM object type</param>
+        static BomItem[] GetSumQuantities(BomItem bom)
+        {
+            List<BomItem> componentSums = new List<BomItem>();
+            BomItem[] allComponents = GetAllComponents(bom).ToArray();
+            foreach (BomItem bomItem in allComponents)
+            {
+                int foundIndex = componentSums.FindIndex(x => x.description == bomItem.description);
+                if (foundIndex > -1)
+                {
+                    componentSums[foundIndex].quantity += bomItem.quantity;
+                }
+                else
+                {
+                    componentSums.Add(bomItem);
+                }
+            }
+            return componentSums.ToArray();
+        }
+
+        /// <summary>
+        /// Writes all total component summed quantities to the file 'output.csv'.
+        /// </summary>
+        /// <param name="bom">BOM object type</param>
+        static void WriteSumOutput(BomItem bom)
+        {
+            BomItem[] providedComponents = GetSumQuantities(bom).Where(x => x.source.ToLower().Equals("provided")).ToArray();
+            string fileContent = "component,quantity\n";
+            foreach (BomItem component in providedComponents)
+            {
+                fileContent += string.Format("{0},{1}\n", component.description, component.quantity);
+            }
+            File.WriteAllText("output.csv", fileContent);
+        }
+
+        /// <summary>
+        /// Prints all components with unprovided subcomponents to console.
+        /// </summary>
+        /// <param name="bom">BOM object type</param>
+        static void UnprovidedComponentSteps(BomItem bom)
+        {
+            BomItem[] unprovidedSteps = GetStepsWithUnprovidedComponents(bom).ToArray();
+            foreach (BomItem component in unprovidedSteps)
+            {
+                Console.WriteLine("Step {0} '{1}' has no provided components added.", component.step, component.description);
+            }
+        }
+
+        /// <summary>
+        /// Calculates and prints the total takt time of the given BOM based on its provided routings.
+        /// </summary>
+        /// <param name="bom">BOM object type</param>
+        /// <param name="routings">RoutingItem array</param>
+        static void CalculateTaktTime(BomItem bom, RoutingItem[] routings)
+        {
+            BomItem[] allComponents = GetAllComponents(bom);
+            RoutingItem[] componentRoutings = routings.Where(x => allComponents.Any(o => o.step == x.step)).ToArray(); // filter out routings not apart of the bom
+            int totalSeconds = componentRoutings.Sum(x => x.taktTime);
+            double totalMinutes = totalSeconds / 60.0;
+            Console.WriteLine("Overall takt time: {0} minutes", string.Format("{0:0.00}", totalMinutes));
+        }
+
+        static void ProgramEnd(string message)
+        {
+            Console.WriteLine("\n" + message + "\n");
+            Console.WriteLine("Press any key to exit:");
+            Console.ReadKey();
+        }
+
+        static void Main()
+        {
+            try
+            {
+                FileStream bomFile = File.OpenRead("bunkbed-bom.json");
+                FileStream routingFile = File.OpenRead("bunkbed-routing.json");
+                BomItem bom = JsonSerializer.Deserialize<BomItem>(bomFile) ?? throw new Exception("Failed to parse 'bunkbed-bom.json'.");
+                RoutingItem[] routings = JsonSerializer.Deserialize<RoutingItem[]>(routingFile) ?? throw new Exception("Failed to parse 'bunkbed-routing.json'.");
+                WriteSumOutput(bom);
+                UnprovidedComponentSteps(bom);
+                CalculateTaktTime(bom, routings);
+                ProgramEnd("Bunk Bed Build Takt Time Calculator finished successfully.");
+            }
+            catch (Exception e)
+            {
+                ProgramEnd("Error running Bunk Bed Build Takt Time Calculator.\n" + e.Message);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,10 +1,60 @@
-## MES Code Challenges
+# Important
 
-This is a repository holding all of Noah Krueger's submissions for the MES code challenges.
+The JSON files in the project ('bunkbed-bom.json' and 'bunkbed-routing.json') are copied to the build folder on compilation.
+If you do not want this to happen every time building the project, you can remove the setting found in the '.csproj' file.
 
-## Where to find the submissions
-#### The project submissions can be found on their respective branches:
+# Bunk Bed Build - Takt Time Calculator
 
-- Bunk Bed BOM Routing: `bunkbed-bom-routing`
-- Config API: `config-api`
-- Barcode Generator: `blazor-barcode-generator`
+Although Gentex doesn't manufacture furniture, configuration for materials needed to build things and instructions for how to build them are used the same way. Like products assembled at Gentex, a bunk bed has many small components that are assembled together, then those are further assembled together forming a heirarchy from hardware and wood to a finished bed called a bill of material or BOM.
+
+Also like Gentex products, bunk beds are put together following an ordered set of instructions. In manufacturing this is called a routing. Measuring time to build something and comparing it to expected assembly time, called takt time, also helps to ensure efficiency and quality.
+
+For this excercise, engineers would like a .NET console application that can output a flat list for the sum of all 'Provided' components and calculate an overall takt time given a bill of material and a routing.
+
+# Requirements
+
+Use [bunkbed-bom.json](./bunkbed-bom.json) and [bunkbed-routing.json](./;bunkbed-routing.json) to build a console app and complete the following:
+
+1. Put all of the provided components and their sum quantity in a flat list and write it to a text file.
+
+   output.csv
+
+   ```
+   component,quantity
+   {Description1},{Quantity1}
+   {Description2},{Quantity2}
+   ...
+   ```
+
+1. Write to console if there are any steps where there are no provided components being added.
+   ```
+   Step {Step} '{Step Description}' has no provided components added.
+   ```
+1. Write to console the overall takt time (values expressed in minutes).
+   ```
+   Overall takt time: {time} minutes.
+   ```
+
+# Tools Needed
+
+Use a text editor like [VS Code](https://code.visualstudio.com/).
+
+Verify dotnet is installed, or install from [here](https://dotnet.microsoft.com/en-us/download):
+
+```bash
+dotnet --info
+```
+
+# Getting Started
+
+Scaffold out a new dotnet webapi from a console or VS Code terminal:
+
+```bash
+dotnet new console
+```
+
+Run the api from the console/terminal:
+
+```bash
+dotnet run
+```
